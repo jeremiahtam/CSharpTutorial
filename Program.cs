514fc6d14ed6aadc201266170b0b112a5c5128a1
Esite Jeremiah Tamarankuroemi <jerry4destinychild@yahoo.com>
2025-11-29 01:53:20 +0100

    Directory manipulation

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿
-class Program
+﻿namespace Tutorial
 {
-    static void Main(string[] args)
-    {       
-        Console.WriteLine("Hello, World!");
-    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo currentDir = new(Directory.GetCurrentDirectory());
+            DirectoryInfo userDir = new(@"C:\Users\jerry");
 
+            Console.WriteLine(currentDir);
+            Console.WriteLine(userDir.Name);
+            Console.WriteLine(userDir.FullName);
+            Console.WriteLine(userDir.Parent);
+            Console.WriteLine(userDir.CreationTime);
+            Console.WriteLine(currentDir.Root);
+            DirectoryInfo toCreate = new(@"C:\CSharpProjects\CSharpTutorial\FileStorage");
+            toCreate.Create();
+        }
+    }
 }
