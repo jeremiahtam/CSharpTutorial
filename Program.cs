4be312db43f8c8ed51ed61c560f2ce074326ebf5
Esite Jeremiah Tamarankuroemi <jerry4destinychild@yahoo.com>
2025-11-30 02:16:50 +0100

    Directory manipulation

--- Program.cs
+++ Program.cs
@@ -15,6 +15,17 @@
             Console.WriteLine(currentDir.Root);
             DirectoryInfo toCreate = new(@"C:\CSharpProjects\CSharpTutorial\FileStorage");
             toCreate.Create();
+            //Directory.Delete(@"C:\CSharpProjects\CSharpTutorial\FileStorage");
+            string[] customers = { "Tom Smith", "Bob Smith", "Alice Smith" };
+            string textFilePath = @"C:\CSharpProjects\CSharpTutorial\FileStorage\textFile1.txt";
+            File.WriteAllLines(textFilePath, customers);
+            foreach (string customer in File.ReadAllLines(textFilePath))
+            {
+                Console.WriteLine($"customer: {customer}");
+            }
+            DirectoryInfo myDataDir = new(@"C:\CSharpProjects\CSharpTutorial\FileStorage");
+            FileInfo[] files = myDataDir.GetFiles("*.txt", SearchOption.AllDirectories);
+
         }
     }
 }
